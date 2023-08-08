d216e3c7690c2bc8f8e60aa2a71081d399e64bbd
makendyalexis <141700056+makendyalexis@users.noreply.github.com>
2023-08-07 23:38:35 -0400

    Add files via upload

---
+++ enonce4.py
@@ -0,0 +1,12 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x = 8.4
+>>> y = 4
+>>> print(x + y)
+12.4
+>>> print(x - y)
+4.4
+>>> print(x * y)
+33.6
+>>> print(x / y)
+2.1
---
+++ enonce5.py
@@ -0,0 +1,14 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> w = "RAGNAR"
+>>> print(type(w))
+<class 'str'>
+>>> x = 6
+>>> print(type(x))
+<class 'int'>
+>>> y = 4.7
+>>> print(type(y))
+<class 'float'>
+>>> z = True
+>>> print(type(z))
+<class 'bool'>
